b991d6019d801338e4b0fc099add89fa0dc56c31
zhuam <zhuam@foxmail.com>
2020-09-15 15:45:18 +0800

    rt

--- src/main/java/com/feeyo/net/codec/http/websocket/WsEncoder.java
+++ src/main/java/com/feeyo/net/codec/http/websocket/WsEncoder.java
@@ -5,25 +5,24 @@ import java.nio.ByteBuffer;
 public class WsEncoder {
 	
 	public ByteBuffer encode(Frame frame) {
-		int length = 1; // 固定有一个命令码，占一位
-		length += frame.getPayloadLength();
 		//
-		byte header0 = (byte) (0x8f & (frame.getOpCode() | 0xf0));
+		int length = frame.getPayloadLength() + 1;
+		byte b0 = (byte) (0x8f & (frame.getOpCode() | 0xf0));
 		ByteBuffer buffer = null;
 		if (length < 126) {
 			buffer = ByteBuffer.allocate(2 + length);
-			buffer.put(header0);
+			buffer.put(b0);
 			buffer.put((byte) length);
 		} else if (length < (1 << 16) - 1) {
 			buffer = ByteBuffer.allocate(4 + length);
-			buffer.put(header0);
+			buffer.put(b0);
 			buffer.put((byte) 126);
 			// 
 			buffer.put((byte) (length >>> 8));
 			buffer.put((byte) (length & 0xff));
 		} else {
 			buffer = ByteBuffer.allocate(10 + length);
-			buffer.put(header0);
+			buffer.put(b0);
 			buffer.put((byte) 127);
 			buffer.put(new byte[] { 0, 0, 0, 0 });
 			//
