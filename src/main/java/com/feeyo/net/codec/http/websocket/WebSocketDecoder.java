b8fff94e0ef1f486ab1f4e01c785c1882fde8332
zhuam <zhuam@foxmail.com>
2020-09-15 17:52:48 +0800

    rt

--- src/main/java/com/feeyo/net/codec/http/websocket/WebSocketDecoder.java
+++ src/main/java/com/feeyo/net/codec/http/websocket/WebSocketDecoder.java
@@ -23,7 +23,6 @@ public class WebSocketDecoder implements Decoder<Frame> {
 	//
 	// Frame
 	private Frame frame;
-	private boolean priorDataFrame;
 	//
 	// payload specific
 	private ByteBuffer payload;
@@ -37,125 +36,75 @@ public class WebSocketDecoder implements Decoder<Frame> {
 
 	@Override
 	public Frame decode(byte[] buf) throws UnknownProtocolException {
-		//
 		ByteBuffer buffer = ByteBuffer.wrap(buf);
 		while (buffer.hasRemaining()) {
 			switch (state) {
-			case START: {
-				// peek at byte
-				byte b = buffer.get();
-				boolean fin = ((b & 0x80) != 0);
-				byte opcode = (byte) (b & 0x0F);
-				if (!OpCode.isKnown(opcode)) {
-					throw new UnknownProtocolException("Unknown opcode: " + opcode);
+				case START: {
+					// peek at byte
+					byte b = buffer.get();
+					boolean fin = ((b & 0x80) != 0);
+					byte opcode = (byte) (b & 0x0F);
+					if (!OpCode.isKnown(opcode)) {
+						throw new UnknownProtocolException("Unknown opcode: " + opcode);
+					}
+					//
+					frame = new Frame(opcode);
+					frame.setFin(fin);
+					//
+					// Are any flags set?
+					if ((b & 0x70) != 0) {
+						//
+						if ((b & 0x40) != 0) {
+							if (isRsv1InUse())
+								frame.setRsv1(true);
+							else
+								throw new UnknownProtocolException("RSV1 not allowed to be set");
+						}
+						if ((b & 0x20) != 0) {
+							if (isRsv2InUse())
+								frame.setRsv2(true);
+							else
+								throw new UnknownProtocolException("RSV2 not allowed to be set");
+						}
+						if ((b & 0x10) != 0) {
+							if (isRsv3InUse())
+								frame.setRsv3(true);
+							else
+								throw new UnknownProtocolException("RSV3 not allowed to be set");
+						}
+					}
+	
+					state = State.PAYLOAD_LEN;
+					break;
 				}
 				//
-				// base framing flags
-				switch ( opcode ) {
-				case OpCode.TEXT:
-				case OpCode.BINARY:
-					frame = new Frame(opcode);
-					// data validation
-					if (priorDataFrame) {
-						throw new UnknownProtocolException("Unexpected " + OpCode.name(opcode) + " frame, was expecting CONTINUATION");
+				case PAYLOAD_LEN: {
+					byte b = buffer.get();
+					frame.setMasked((b & 0x80) != 0);
+					payloadLength = (byte) (0x7F & b);
+					if (payloadLength == 127) // 0x7F
+					{
+						// length 8 bytes (extended payload length)
+						payloadLength = 0;
+						state = State.PAYLOAD_LEN_BYTES;
+						cursor = 8;
+						break; 
+					} else if (payloadLength == 126) // 0x7E
+					{
+						// length 2 bytes (extended payload length)
+						payloadLength = 0;
+						state = State.PAYLOAD_LEN_BYTES;
+						cursor = 2;
+						break; 
 					}
-					break;
-				case OpCode.CONTINUATION:
-					frame = new Frame(opcode);
-					// continuation validation
-					if (!priorDataFrame) {
-						throw new UnknownProtocolException("CONTINUATION frame without prior !FIN");
-					}
-					// Be careful to use the original opcode
-					break;
-				case OpCode.CLOSE:
-				case OpCode.PING:
-				case OpCode.PONG:
-					frame = new Frame(opcode);
-					// control frame validation
-					if (!fin) {
-						throw new UnknownProtocolException("Fragmented Frame [" + OpCode.name(opcode) + "]");
-					}
-					break;
-				}
-
-				frame.setFin(fin);
-				//
-				// Are any flags set?
-				if ((b & 0x70) != 0) {
-					/*
-					 * RFC 6455 Section 5.2
-					 */
-					if ((b & 0x40) != 0) {
-						if (isRsv1InUse())
-							frame.setRsv1(true);
-						else
-							throw new UnknownProtocolException("RSV1 not allowed to be set");
-					}
-					if ((b & 0x20) != 0) {
-						if (isRsv2InUse())
-							frame.setRsv2(true);
-						else
-							throw new UnknownProtocolException("RSV2 not allowed to be set");
-					}
-					if ((b & 0x10) != 0) {
-						if (isRsv3InUse())
-							frame.setRsv3(true);
-						else
-							throw new UnknownProtocolException("RSV3 not allowed to be set");
-					}
-				}
-
-				state = State.PAYLOAD_LEN;
-				break;
-			}
-
-			case PAYLOAD_LEN: {
-				byte b = buffer.get();
-				frame.setMasked((b & 0x80) != 0);
-				payloadLength = (byte) (0x7F & b);
-				if (payloadLength == 127) // 0x7F
-				{
-					// length 8 bytes (extended payload length)
-					payloadLength = 0;
-					state = State.PAYLOAD_LEN_BYTES;
-					cursor = 8;
-					break; // continue onto next state
-				} else if (payloadLength == 126) // 0x7E
-				{
-					// length 2 bytes (extended payload length)
-					payloadLength = 0;
-					state = State.PAYLOAD_LEN_BYTES;
-					cursor = 2;
-					break; // continue onto next state
-				}
-				assertSanePayloadLength(payloadLength);
-				if (frame.isMasked()) {
-					state = State.MASK;
-				} else {
-					// special case for empty payloads (no more bytes left in
-					// buffer)
-					if (payloadLength == 0) {
-						state = State.START;
-						return frame;
-					}
-					resetMask(frame.getMask());
-					state = State.PAYLOAD;
-				}
-				break;
-			}
-
-			case PAYLOAD_LEN_BYTES: {
-				byte b = buffer.get();
-				--cursor;
-				payloadLength |= (b & 0xFF) << (8 * cursor);
-				if (cursor == 0) {
-					assertSanePayloadLength(payloadLength);
+					
+					if (payloadLength > Integer.MAX_VALUE) 
+						throw new UnknownProtocolException("[int-sane!] cannot handle payload lengths larger than " + Integer.MAX_VALUE);
+					//
 					if (frame.isMasked()) {
 						state = State.MASK;
 					} else {
-						// special case for empty payloads (no more bytes left
-						// in buffer)
+						// 空有效负载的特殊情况（缓冲区中不再有字节）
 						if (payloadLength == 0) {
 							state = State.START;
 							return frame;
@@ -163,60 +112,80 @@ public class WebSocketDecoder implements Decoder<Frame> {
 						resetMask(frame.getMask());
 						state = State.PAYLOAD;
 					}
+					break;
 				}
-				break;
-			}
-
-			case MASK: {
-				byte m[] = new byte[4];
-				frame.setMask(m);
-				if (buffer.remaining() >= 4) {
-					buffer.get(m, 0, 4);
-					// special case for empty payloads (no more bytes left in
-					// buffer)
-					if (payloadLength == 0) {
+				//
+				case PAYLOAD_LEN_BYTES: {
+					byte b = buffer.get();
+					--cursor;
+					payloadLength |= (b & 0xFF) << (8 * cursor);
+					if (cursor == 0) {
+						//
+						if (payloadLength > Integer.MAX_VALUE) 
+							throw new UnknownProtocolException("[int-sane!] cannot handle payload lengths larger than " + Integer.MAX_VALUE);
+						//
+						if (frame.isMasked()) {
+							state = State.MASK;
+						} else {
+							// 空有效负载的特殊情况（缓冲区中不再有字节）
+							if (payloadLength == 0) {
+								state = State.START;
+								return frame;
+							}
+							resetMask(frame.getMask());
+							state = State.PAYLOAD;
+						}
+					}
+					break;
+				}
+	
+				case MASK: {
+					byte m[] = new byte[4];
+					frame.setMask(m);
+					if (buffer.remaining() >= 4) {
+						buffer.get(m, 0, 4);
+						// 空有效负载的特殊情况（缓冲区中不再有字节）
+						if (payloadLength == 0) {
+							state = State.START;
+							return frame;
+						}
+						resetMask(frame.getMask());
+						state = State.PAYLOAD;
+					} else {
+						state = State.MASK_BYTES;
+						cursor = 4;
+					}
+					break;
+				}
+				//
+				case MASK_BYTES: {
+					byte b = buffer.get();
+					frame.getMask()[4 - cursor] = b;
+					--cursor;
+					if (cursor == 0) {
+						// 空有效负载的特殊情况（缓冲区中不再有字节）
+						if (payloadLength == 0) {
+							state = State.START;
+							return frame;
+						}
+						resetMask(frame.getMask());
+						state = State.PAYLOAD;
+					}
+					break;
+				}
+				//
+				case PAYLOAD: {
+					frame.assertValid();
+					if (parsePayload(buffer)) {
+						//
 						state = State.START;
+						// we have a frame!
 						return frame;
 					}
-					resetMask(frame.getMask());
-					state = State.PAYLOAD;
-				} else {
-					state = State.MASK_BYTES;
-					cursor = 4;
+					break;
 				}
-				break;
-			}
-
-			case MASK_BYTES: {
-				byte b = buffer.get();
-				frame.getMask()[4 - cursor] = b;
-				--cursor;
-				if (cursor == 0) {
-					// special case for empty payloads (no more bytes left in
-					// buffer)
-					if (payloadLength == 0) {
-						state = State.START;
-						return frame;
-					}
-					resetMask(frame.getMask());
-					state = State.PAYLOAD;
-				}
-				break;
-			}
-
-			case PAYLOAD: {
-				frame.assertValid();
-				if (parsePayload(buffer)) {
-					//
-					state = State.START;
-					// we have a frame!
-					return frame;
-				}
-				break;
-			}
 			}
 		}
-		//
 		return null;
 	}
 
@@ -226,9 +195,6 @@ public class WebSocketDecoder implements Decoder<Frame> {
 		}
 		//
 		if (buffer.hasRemaining()) {
-			// Create a small window of the incoming buffer to work with.
-			// this should only show the payload itself, and not any more
-			// bytes that could belong to the start of the next frame.
 			int bytesSoFar = payload == null ? 0 : payload.position();
 			int bytesExpected = payloadLength - bytesSoFar;
 			int bytesAvailable = buffer.remaining();
@@ -300,12 +266,6 @@ public class WebSocketDecoder implements Decoder<Frame> {
 		}
 		this.maskInt = maskInt;
 		this.maskOffset = 0;
-	}
-
-	private void assertSanePayloadLength(long len) throws UnknownProtocolException {
-		if (len > Integer.MAX_VALUE) {
-			throw new UnknownProtocolException("[int-sane!] cannot handle payload lengths larger than " + Integer.MAX_VALUE);
-		}
 	}
 
 	public boolean isRsv1InUse() {
