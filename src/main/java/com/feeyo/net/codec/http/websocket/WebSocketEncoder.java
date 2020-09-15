72bcf0e392babd5eb4c95e4cf6b7bc57cb9c11c5
zhuam <zhuam@foxmail.com>
2020-09-15 16:07:49 +0800

    rt

--- src/main/java/com/feeyo/net/codec/http/websocket/WebSocketEncoder.java
+++ src/main/java/com/feeyo/net/codec/http/websocket/WebSocketEncoder.java
@@ -5,7 +5,6 @@ import java.nio.ByteBuffer;
 public class WebSocketEncoder {
 	
 	public ByteBuffer encode(Frame frame) {
-		//
 		int length = frame.getPayloadLength();
 		byte b0 = (byte) (0x8f & (frame.getOpCode() | 0xf0));
 		ByteBuffer buffer = null;
@@ -13,21 +12,19 @@ public class WebSocketEncoder {
 			buffer = ByteBuffer.allocate(2 + length);
 			buffer.put(b0);
 			buffer.put((byte) length);
-			
+			//
 		} else if (length < (1 << 16) - 1) {
 			buffer = ByteBuffer.allocate(4 + length);
 			buffer.put(b0);
-			buffer.put((byte) 126);
-			// 
+			buffer.put((byte) 126); 
 			buffer.put((byte) (length >>> 8));
 			buffer.put((byte) (length & 0xff));
-			
+			//
 		} else {
 			buffer = ByteBuffer.allocate(10 + length);
 			buffer.put(b0);
 			buffer.put((byte) 127);
 			buffer.put(new byte[] { 0, 0, 0, 0 });
-			//
 			buffer.put((byte) (length >>> 24));
 			buffer.put((byte) (length >>> 16));
 			buffer.put((byte) (length >>> 8));
