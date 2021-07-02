f14c0bbd4b43d102608a2c6171414c81a773ed44
zhuam <zhuam@foxmail.com>
2021-07-02 14:24:35 +0800

    rt

--- src/main/java/com/feeyo/net/codec/http/websocket/WebSocketEncoder.java
+++ src/main/java/com/feeyo/net/codec/http/websocket/WebSocketEncoder.java
@@ -8,7 +8,10 @@ import com.feeyo.net.nio.NetSystem;
 
 public class WebSocketEncoder {
 	//
+	// @see https://datatracker.ietf.org/doc/html/rfc6455
 	// @see https://stackoverflow.com/questions/25189006/how-to-frame-websocket-data-in-javascript
+	// @see https://stackoverflow.com/questions/43715746/how-to-encode-binary-data-over-65536-bytes-to-websocket-frame-on-c
+	// @see https://github.com/netty/netty/blob/4.1/codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocket08FrameEncoder.java
 	//
 	public ByteBuffer encode(Frame frame) {
 		if ( frame == null )
@@ -17,33 +20,33 @@ public class WebSocketEncoder {
 		BufferPool bufferPool = NetSystem.getInstance().getBufferPool();
 		ByteBuffer buffer = null;
 		try {
-			int len = frame.getPayloadLength();
-			byte header0 = (byte) (0x8f & (frame.getOpCode() | 0xf0));
-			if (len <= 125) {
-				buffer = bufferPool.allocate(2 + len);
-				buffer.put(header0);
-				buffer.put((byte) len);
+			int payloadLength = frame.getPayloadLength();
+			byte b0 = (byte) (0x8f & (frame.getOpCode() | 0xf0));
+			if (payloadLength <= 125) {
+				buffer = bufferPool.allocate(2 + payloadLength);
+				buffer.put(b0);
+				buffer.put((byte) payloadLength);
 				//
-			} else if (len <= 0xffff) {
-				buffer = bufferPool.allocate(4 + len);
-				buffer.put(header0);
+			} else if (payloadLength <= 0xffff) {
+				buffer = bufferPool.allocate(4 + payloadLength);
+				buffer.put(b0);
 				buffer.put((byte) 126); 
-				buffer.put((byte) (len >>> 8));
-				buffer.put((byte) (len & 0xff));
+				buffer.put((byte) (payloadLength >>> 8));
+				buffer.put((byte) (payloadLength & 0xff));
 				//
 			} else {
 				/* 0xffff < len <= 2^63 */
-				buffer = bufferPool.allocate(10 + len);
-				buffer.put(header0);
+				buffer = bufferPool.allocate(10 + payloadLength);
+				buffer.put(b0);
 				buffer.put((byte) 127);
-				buffer.put((byte)((len >> 56) & 0xff));
-				buffer.put((byte)((len >> 48) & 0xff));
-				buffer.put((byte)((len >> 40) & 0xff));
-				buffer.put((byte)((len >> 32) & 0xff));
-				buffer.put((byte)((len >> 24) & 0xff));
-				buffer.put((byte)((len >> 16) & 0xff));
-				buffer.put((byte)((len >> 8) & 0xff));
-				buffer.put((byte)(len & 0xff));
+				buffer.put((byte)((payloadLength >> 56) & 0xff));
+				buffer.put((byte)((payloadLength >> 48) & 0xff));
+				buffer.put((byte)((payloadLength >> 40) & 0xff));
+				buffer.put((byte)((payloadLength >> 32) & 0xff));
+				buffer.put((byte)((payloadLength >> 24) & 0xff));
+				buffer.put((byte)((payloadLength >> 16) & 0xff));
+				buffer.put((byte)((payloadLength >> 8) & 0xff));
+				buffer.put((byte)(payloadLength & 0xff));
 			}
 			//
 			for (int i = 0; i < frame.getPayloadLength(); i++) {
