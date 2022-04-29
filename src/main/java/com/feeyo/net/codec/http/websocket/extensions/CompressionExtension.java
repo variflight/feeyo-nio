fb62549a14cf8ebf5ff4d9e323918f5b2c7de227
zhuam <zhuam@foxmail.com>
2022-04-29 10:58:10 +0800

    rt

--- src/main/java/com/feeyo/net/codec/http/websocket/extensions/CompressionExtension.java
+++ src/main/java/com/feeyo/net/codec/http/websocket/extensions/CompressionExtension.java
@@ -4,15 +4,18 @@ import com.feeyo.net.codec.UnknownProtocolException;
 import com.feeyo.net.codec.http.websocket.Frame;
 
 public abstract class CompressionExtension extends DefaultExtension {
+	//
 	@Override
 	public void isFrameValid(final Frame inputFrame) throws UnknownProtocolException {
 		if (inputFrame.isDataFrame() && (inputFrame.isRsv2() || inputFrame.isRsv3())) {
-			String msg = String.format("bad rsv RSV1: %s RSV2:%s  RSV3:%s", inputFrame.isRsv1(), inputFrame.isRsv2(), inputFrame.isRsv3());
+			String msg = String.format("bad rsv RSV1: %s RSV2:%s  RSV3:%s", 
+					inputFrame.isRsv1(), inputFrame.isRsv2(), inputFrame.isRsv3());
 			throw new UnknownProtocolException(msg);
 		}
 		//
 		if (inputFrame.isControlFrame() && (inputFrame.isRsv1() || inputFrame.isRsv2() || inputFrame.isRsv3())) {
-			String msg = String.format("bad rsv RSV1: %s RSV2:%s  RSV3:%s", inputFrame.isRsv1(), inputFrame.isRsv2(), inputFrame.isRsv3());
+			String msg = String.format("bad rsv RSV1: %s RSV2:%s  RSV3:%s", 
+					inputFrame.isRsv1(), inputFrame.isRsv2(), inputFrame.isRsv3());
 			throw new UnknownProtocolException(msg);
 		}
 	}
